03f40991148819c4eebc5a3f7772b587f95ff934
unknown <computerking0646@gmail.com>
2023-05-18 00:30:55 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-//버튼에 기능개발을 해보자 & 리액트 state변경하는 법
+//버튼에 기능개발을 해보자 & 리액트 state변경하는 법 dsdjdjd
 function App() {
   let [글제목, b] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState(0);
