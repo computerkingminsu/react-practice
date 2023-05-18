86684719c12dfede03048edd4c1d633dd55cb7b9
unknown <computerking0646@gmail.com>
2023-05-18 14:16:56 +0900

    djd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-//버튼에 기능개발을 해보자 & 리액트 state변경하는 법 git test
+//버튼에 기능개발을 해보자 & 리액트 state변경하는 법 
 function App() {
-  let [글제목, b] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [글제목, 글제목변경] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className="list">
+      <div className="list">  
         <h4>
-          {글제목[0]}<span onClick={() => {따봉변경(따봉+1);}}>👍</span>{따봉}
+          {글제목[0]}<span onClick={() => {따봉변경(따봉+1);}}> 👍</span>{따봉}
         </h4>
         <p>2월 17일 발행</p>
       </div>
@@ -22,11 +23,26 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{
+          modal == true ? setModal(false) : setModal(true);
+        }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      {
+        modal == true ? <Modal/> : null
+      }
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
